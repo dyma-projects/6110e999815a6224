1e99d18744ca1ed2a95e5d8411eb588b6d684ef2
Anjara <anjaranantenainals@gmail.com>
2022-03-30 17:18:02 +0200

    Projet du chapitre 4

--- src/app/exercice1/exercice1-enfant/exercice1-enfant.component.ts
+++ src/app/exercice1/exercice1-enfant/exercice1-enfant.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
 
 @Component({
-  selector: 'app-exercice1-enfant',
-  templateUrl: './exercice1-enfant.component.html',
-  styleUrls: ['./exercice1-enfant.component.css']
+  selector: "app-exercice1-enfant",
+  templateUrl: "./exercice1-enfant.component.html",
+  styleUrls: ["./exercice1-enfant.component.css"],
 })
 export class Exercice1EnfantComponent implements OnInit {
+  @Output() compteurChange = new EventEmitter();
 
-  constructor() { }
+  @Input() public result: number;
 
-  ngOnInit() {
+  constructor() {}
+
+  ngOnInit() {}
+
+  incrementer() {
+    this.result++;
+    this.compteurChange.emit({
+      value: this.result,
+    });
   }
 
+  decrementer() {
+    this.result--;
+    this.compteurChange.emit({
+      value: this.result,
+    });
+  }
 }
